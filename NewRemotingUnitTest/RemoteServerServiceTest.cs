699647a093eb5b9ebec32aa4a34d6dd7fdf4b8fe
Patrick Grawehr <patrick.grawehr@leica-geosystems.com>
2021-06-08 15:13:23 +0200

    Update to new RemoteServerService version

---
+++ NewRemotingUnitTest/RemoteServerServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airborne.Generic.Remote.Loader;
+using Moq;
+using NewRemoting;
+using NUnit.Framework;
+
+namespace NewRemotingUnitTest
+{
+	[TestFixture]
+	internal class RemoteServerServiceTest
+	{
+		private RemoteServerService m_service;
+		private Mock<FileHashCalculator> m_hashCalculatorMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			m_hashCalculatorMock = new Mock<FileHashCalculator>();
+			m_service = new RemoteServerService();
+		}
+
+		[Test]
+		public void ClassIsNotSealed()
+		{
+			// To simplify the remote infrastructure at this point, it must be possible to create a proxy of this class
+			var t = typeof(RemoteServerService);
+			Assert.False(t.IsSealed);
+		}
+	}
+}
