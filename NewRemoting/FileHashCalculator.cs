aaf93f053423c13d6e686144d992572a3bc78511
Patrick Grawehr <patrick.grawehr@leica-geosystems.com>
2021-06-08 15:13:23 +0200

    Update to new RemoteServerService version

---
+++ NewRemoting/FileHashCalculator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Airborne.Generic.Remote.Loader
+{
+	public class FileHashCalculator : IDisposable
+	{
+		private readonly MD5CryptoServiceProvider m_md5;
+
+		public FileHashCalculator()
+		{
+			m_md5 = new MD5CryptoServiceProvider();
+		}
+
+		/// <summary>
+		/// Returns a hash code for a file. Files containing version data mainly use that instead of reading the whole file
+		/// </summary>
+		/// <param name="fileName">File to analyze</param>
+		/// <returns>A byte[] with the hashcode</returns>
+		public virtual byte[] CalculateFastHashFromFile(string fileName)
+		{
+			using var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+			if (stream.Length < 64 * 1024)
+			{
+				return m_md5.ComputeHash(stream);
+			}
+
+			FileVersionInfo info = FileVersionInfo.GetVersionInfo(fileName);
+			bool isExecutable = FileIsExecutable(stream);
+			stream.Position = 0;
+			// if the file is executable or has a version attached, we use that for comparison.
+			if (isExecutable || !string.IsNullOrWhiteSpace(info.FileVersion))
+			{
+				// Create a combined stream of the file version and the file header
+				MemoryStream dataToAnalyze = new MemoryStream();
+				string versionText = FileVersionToString(info);
+				byte[] infoBytes = Encoding.Unicode.GetBytes(versionText);
+				dataToAnalyze.Write(infoBytes, 0, infoBytes.Length);
+				byte[] firstBlock = new byte[1024];
+				stream.Read(firstBlock, 0, firstBlock.Length);
+				dataToAnalyze.Write(firstBlock, 0, firstBlock.Length);
+				dataToAnalyze.Position = 0;
+
+				return m_md5.ComputeHash(dataToAnalyze);
+			}
+
+			// It's a large, unknown file type. We have to go the full way
+			return m_md5.ComputeHash(stream);
+		}
+
+		/// <summary>
+		/// Returns the file this file depends on, if any. If the other file needs updating, this one needs, too.
+		/// This can be used i.e. to keep pdbs in sync with their executables. Calculating the hash of pdbs on the
+		/// other hand is expensive, because they have no version info and are typically large.
+		/// </summary>
+		/// <param name="fileName">File to check</param>
+		/// <returns>The file this depends on or null if no such file is known. Also returns null if the main file
+		/// does not exist</returns>
+		public virtual string GetFileThisDependsOn(string fileName)
+		{
+			FileInfo fi = new FileInfo(fileName);
+			if (string.Compare(fi.Extension, "PDB", StringComparison.OrdinalIgnoreCase) == 0)
+			{
+				string fileToCheck = Path.GetFileNameWithoutExtension(fileName) + ".dll";
+				if (File.Exists(fileToCheck))
+				{
+					return fileToCheck;
+				}
+
+				fileToCheck = Path.GetFileNameWithoutExtension(fileName) + ".exe";
+				if (File.Exists(fileToCheck))
+				{
+					return fileToCheck;
+				}
+
+				return null;
+			}
+
+			return null;
+		}
+
+		private static string FileVersionToString(FileVersionInfo info)
+		{
+			// The default "ToString()" implementation includes the path of the full file, which we do not want to compare
+			StringBuilder b = new StringBuilder();
+			b.AppendLine("FileVersion:" + info.FileVersion);
+			b.AppendLine("Description: " + info.FileDescription);
+			b.AppendLine("OriginalName: " + info.OriginalFilename);
+			b.AppendLine("ProductName: " + info.ProductName);
+			b.AppendLine("ProductVersion: " + info.ProductVersion);
+			b.AppendLine("Comments" + info.Comments);
+			return b.ToString();
+		}
+
+		private static bool FileIsExecutable(Stream stream)
+		{
+			if (stream.Length > 1024)
+			{
+				int first = stream.ReadByte();
+				int second = stream.ReadByte();
+
+				// Magic number: EXE header
+				if (first != 'M' && second != 'Z')
+				{
+					return false;
+				}
+
+				stream.Position = 0x80;
+				first = stream.ReadByte();
+				second = stream.ReadByte();
+				// Second magic number
+				if (first != 'P' && second != 'E')
+				{
+					return false;
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			m_md5.Dispose();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
--- NewRemoting/RemoteServerService.cs
+++ NewRemoting/RemoteServerService.cs
@@ -1,33 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Airborne.Generic.Remote.Loader;
 
 namespace NewRemoting
 {
+	/// <summary>
+	/// Helper services for the remoting infrastructure, including copying files to a remote host.
+	/// </summary>
+	/// <remarks>Note: Do not seal this class!</remarks>
 	public class RemoteServerService : MarshalByRefObject, IRemoteServerService
 	{
-		private readonly MD5CryptoServiceProvider _cryptographyFactory;
-		private readonly DirectoryInfo _root;
-		private List<FileInfo> _uploadedFiles;
-		private List<FileInfo> _existingFiles;
+		private readonly DirectoryInfo m_root;
+		private readonly List<FileInfo> m_existingFiles;
+		private readonly FileHashCalculator m_fileHashCalculator;
+		private List<FileInfo> m_uploadedFiles;
 
 		public RemoteServerService()
+			: this(new FileHashCalculator())
 		{
-			_cryptographyFactory = new MD5CryptoServiceProvider();
-			_uploadedFiles = new List<FileInfo>();
-			_root = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-			_existingFiles = ScanExistingFiles(_root, new List<FileInfo>());
 		}
 
-		public void UploadFile(string relativePath, byte[] hash, Stream content)
+		public RemoteServerService(FileHashCalculator fileHashCalculator)
+		{
+			m_fileHashCalculator = fileHashCalculator ?? throw new ArgumentNullException(nameof(fileHashCalculator));
+			m_root = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+			m_existingFiles = ScanExistingFiles(m_root, new List<FileInfo>());
+			m_uploadedFiles = new List<FileInfo>();
+		}
+
+		#region Unit Test
+		[Obsolete("Unit test only")]
+		internal List<FileInfo> UploadedFiles
+		{
+			get => m_uploadedFiles;
+			set => m_uploadedFiles = value;
+		}
+		#endregion
+
+		public virtual bool IsSingleRemoteLoaderInstance()
+		{
+			try
+			{
+				var otherprocesses = OtherRemoteLoaders();
+				return !otherprocesses.Any();
+			}
+			catch (Win32Exception)
+			{
+				// It was apparently not possible to enumerate the processes, this may happen if
+				// another process exists but doesn't belong to the same user.
+				return false;
+			}
+		}
+
+		private static IEnumerable<Process> OtherRemoteLoaders()
+		{
+			var loaderWithoutExt = Path.GetFileNameWithoutExtension(Server.ServerExecutableName);
+			var processes = Process.GetProcessesByName(loaderWithoutExt);
+			var otherprocesses = processes.Where(x => x.HasExited == false && x.Id != Process.GetCurrentProcess().Id);
+			return otherprocesses;
+		}
+
+		public virtual void UploadFile(string relativePath, byte[] hash, Stream content)
 		{
 			// Create desination path for this system. We use current assembly path as root
-			var dest = Path.Combine(_root.FullName, relativePath);
+			var dest = Path.Combine(m_root.FullName, relativePath);
 			var dir = new DirectoryInfo(Path.GetDirectoryName(dest));
 
 			if (!dir.Exists)
@@ -36,20 +82,15 @@ namespace NewRemoting
 			}
 
 			var fi = new FileInfo(dest);
-			_uploadedFiles.Add(fi);
+			m_uploadedFiles.Add(fi);
 
 			if (fi.Exists)
 			{
-				using (var stream = fi.OpenRead())
+				byte[] hashLocal = m_fileHashCalculator.CalculateFastHashFromFile(fi.FullName);
+				if (hashLocal.SequenceEqual(hash))
 				{
-					// Calculate hash from existing file and compare if an upload is needed
-					var hashLocal = _cryptographyFactory.ComputeHash(stream);
-					if (hashLocal.SequenceEqual(hash))
-					{
-						// no upload needed, file is already up to date!
-						return;
-					}
-
+					// no upload needed, file is already up to date!
+					return;
 				}
 			}
 
@@ -60,12 +101,12 @@ namespace NewRemoting
 			}
 		}
 
-		public void UploadFinished()
+		public virtual void UploadFinished()
 		{
 			// Remove all entries that have been uploaded in this session
-			_existingFiles.RemoveAll(x => _uploadedFiles.Any(y => x.FullName.Equals(y.FullName, StringComparison.OrdinalIgnoreCase)));
+			m_existingFiles.RemoveAll(x => m_uploadedFiles.Any(y => x.FullName.Equals(y.FullName, StringComparison.OrdinalIgnoreCase)));
 			// The remaining files in list can be deleted, they where never uploaded
-			foreach (FileInfo fileInfo in _existingFiles)
+			foreach (FileInfo fileInfo in m_existingFiles)
 			{
 				try
 				{
