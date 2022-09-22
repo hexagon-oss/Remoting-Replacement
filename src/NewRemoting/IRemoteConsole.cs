2f23401976094923086329e2a5e716ea5681cff2
Lukas Vonblon <lukas.vonblon@leica-geosystems.com>
2022-09-22 10:28:44 +0200

    Add process wrapper factory to allow mocking

--- src/NewRemoting/IRemoteConsole.cs
+++ src/NewRemoting/IRemoteConsole.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace NewRemoting
+﻿namespace NewRemoting
 {
 	public interface IRemoteConsole
 	{
@@ -9,12 +7,15 @@ namespace NewRemoting
 		/// The process does not forward input, output or error stream, because the forwarding needs to be set up before the process is started.
 		/// To use that functionality see <see cref="CreateProcess"/>.
 		/// </summary>
-		Process LaunchProcess(string commandLine, bool enableUserInterfaceInteraction = false);
+		IProcess LaunchProcess(string commandLine, bool enableUserInterfaceInteraction = false);
 
 		/// <summary>
 		/// Creates a process on the remote machine which can be launched later on.
 		/// Input, output and error streams can be redirected.
 		/// </summary>
-		Process CreateProcess(string commandLine, bool enableUserInterfaceInteraction = false, string fileListPath = null, string workingDirectory = null, bool redirectStandardOutput = false, bool redirectStandardError = false, bool redirectStandardInput = false, bool fireAndForget = false, bool useCsrc = false, string paexec_args = null);
+		IProcess CreateProcess(string commandLine, bool enableUserInterfaceInteraction = false,
+			string fileListPath = null, string workingDirectory = null, bool redirectStandardOutput = false,
+			bool redirectStandardError = false, bool redirectStandardInput = false, bool fireAndForget = false,
+			bool useCsrc = false, string paexec_args = null);
 	}
 }
