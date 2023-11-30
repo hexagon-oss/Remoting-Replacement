74cbba8e9dd170fe21d4082827dcb82b2a622b36
Patrick Grawehr <patrick.grawehr@leica-geosystems.com>
2023-11-30 09:26:49 +0100

    Update build settings

--- src/NewRemoting/FormatterFactory.cs
+++ src/NewRemoting/FormatterFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -6,6 +7,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
@@ -16,21 +19,17 @@ namespace NewRemoting
 	internal class FormatterFactory : SurrogateSelector, ISurrogateSelector
 	{
 		private readonly InstanceManager _instanceManager;
-		private readonly ProxySurrogate _serializationSurrogate;
-		private readonly CustomSerializerSurrogate _customSerializer;
-		private readonly ConcurrentDictionary<string, BinaryFormatter> _cusBinaryFormatters;
+		private readonly ConcurrentDictionary<string, JsonSerializerOptions> _cusBinaryFormatters;
 		private readonly ConcurrentDictionary<int, ManualSerializerSurrogate> _manualSerializers;
 
 		public FormatterFactory(InstanceManager instanceManager)
 		{
 			_instanceManager = instanceManager;
-			_serializationSurrogate = new ProxySurrogate(_instanceManager);
-			_customSerializer = new CustomSerializerSurrogate();
-			_cusBinaryFormatters = new ConcurrentDictionary<string, BinaryFormatter>();
+			_cusBinaryFormatters = new ConcurrentDictionary<string, JsonSerializerOptions>();
 			_manualSerializers = new ConcurrentDictionary<int, ManualSerializerSurrogate>();
 		}
 
-		public IFormatter CreateOrGetFormatter(string otherSideProcessId)
+		public JsonSerializerOptions CreateOrGetFormatter(string otherSideProcessId)
 		{
 			if (_cusBinaryFormatters.TryGetValue(otherSideProcessId, out var formatter))
 			{
@@ -38,6 +37,14 @@ namespace NewRemoting
 			}
 
 			// Doing this twice doesn't hurt (except for a very minor performance penalty)
+			JsonSerializerOptions options = new JsonSerializerOptions()
+			{
+				IncludeFields = true,
+				Converters =
+				{
+					new ProxySurrogate(_instanceManager, otherSideProcessId)
+				}
+			};
 			var bf = new BinaryFormatter(this, new StreamingContext(StreamingContextStates.All, otherSideProcessId));
 			_cusBinaryFormatters.TryAdd(otherSideProcessId, bf);
 			return bf;
@@ -95,13 +102,98 @@ namespace NewRemoting
 			}
 		}
 
-		private sealed class ProxySurrogate : ISerializationSurrogate
+		private sealed class ProxySurrogate : JsonConverter<object>
 		{
 			private readonly InstanceManager _instanceManager;
+			private readonly string _otherSideInstanceId;
 
-			public ProxySurrogate(InstanceManager instanceManager)
+			public ProxySurrogate(InstanceManager instanceManager, string otherSideInstanceId)
 			{
 				_instanceManager = instanceManager;
+				_otherSideInstanceId = otherSideInstanceId;
+			}
+
+			public override bool CanConvert(Type typeToConvert)
+			{
+				return base.CanConvert(typeToConvert) && typeToConvert.IsAssignableTo(typeof(MarshalByRefObject));
+			}
+
+			public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+			{
+				string objectId;
+				if (Client.IsRemoteProxy(value))
+				{
+					// This should have an unit test, but I have not yet found out what test code causes this situation
+					if (!_instanceManager.TryGetObjectId(value, out objectId, out Type originalType))
+					{
+						throw new RemotingException("Couldn't find matching objectId, although should be there");
+					}
+
+					writer.WriteStartObject();
+					writer.WriteString("AssemblyQualifiedName", string.Empty);
+					writer.WriteString("ObjectId", objectId);
+					writer.WriteEndObject();
+				}
+				else
+				{
+					objectId = _instanceManager.RegisterRealObjectAndGetId(value, _otherSideInstanceId);
+					writer.WriteStartObject();
+					writer.WriteString("AssemblyQualifiedName", value.GetType().AssemblyQualifiedName);
+					writer.WriteString("ObjectId", objectId);
+					writer.WriteEndObject();
+				}
+			}
+
+			public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+				if (reader.TokenType != JsonTokenType.StartObject)
+				{
+					throw new JsonException($"Expected object, got {reader.TokenType}");
+				}
+
+				string objectId = null;
+				string typeName = null;
+				while (reader.Read())
+				{
+					if (reader.TokenType == JsonTokenType.EndObject)
+					{
+						break;
+					}
+
+					string propertyName = reader.GetString()!;
+					string propertyValue = reader.GetString();
+					if (propertyName.Equals("ObjectId", StringComparison.Ordinal))
+					{
+						objectId = propertyValue;
+					}
+					else if (propertyName.Equals("AssemblyQualifiedName", StringComparison.Ordinal))
+					{
+						typeName = propertyValue;
+					}
+					else
+					{
+						throw new JsonException($"Unknown property {propertyName} seen");
+					}
+				}
+
+				if (objectId == null || typeName == null)
+				{
+					throw new JsonException("Invalid reference found");
+				}
+
+				// We don't know better here. We do not know what the static type of the field is that will store this reference.
+				object newProxy;
+				if (!string.IsNullOrEmpty(typeName))
+				{
+					Type targetType = Server.GetTypeFromAnyAssembly(typeName);
+					newProxy = _instanceManager.CreateOrGetProxyForObjectId(null, false, targetType, typeName, objectId);
+				}
+				else
+				{
+					newProxy = _instanceManager.CreateOrGetProxyForObjectId(null, false, null, typeName, objectId);
+				}
+
+				return newProxy;
 			}
 
 			public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
