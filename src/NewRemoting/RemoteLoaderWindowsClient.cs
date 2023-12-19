433b5438692b6f991734356ab3635e0b74b57ca7
MALAGOUEN Abdoullahi <abdoullahi.malagouen@leica-geosystems.com>
2023-12-19 15:12:52 +0100

    increase timeout which check if rs process exist

--- src/NewRemoting/RemoteLoaderWindowsClient.cs
+++ src/NewRemoting/RemoteLoaderWindowsClient.cs
@@ -20,7 +20,7 @@ namespace NewRemoting
 		private readonly Func<FileInfo, bool> _shouldFileBeUploadedFunc;
 		private readonly FileHashCalculator _fileHashCalculator;
 		private readonly string _extraArguments;
-		private readonly TimeSpan _remoteProcessConnectionTimeout = TimeSpan.FromSeconds(20);
+		private readonly TimeSpan _remoteProcessConnectionTimeout = TimeSpan.FromSeconds(120);
 
 		private IRemoteServerService _remoteServer;
 		private Client _remotingClient;
